482ee121e23c1d92372b0a26477589c6f1f8764e
princealexandre <princealexandre2000@gmail.com>
2025-04-24 23:44:11 -0400

    A: Code a function to print the text of the Frankenstein book to the console

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open (filepath) as f: 
+        return f.read()
+def main():
+    book_to_analyze = get_book_text("books/frankenstein.txt")
+    print (book_to_analyze)
+main ()
