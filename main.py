005b72625bb28bfa29e5446a4ffa08b1daab4fa7
princealexandre <princealexandre2000@gmail.com>
2025-05-01 17:37:30 -0400

    D: Completed the main script to display according to the requested format in the boot.dev task

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ def main():
     word_counts = get_num_words(text)
     character_counts = count_characters(text)
     sorted_characters = turn_dictionary_into_sorted_list (character_counts)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_counts} total words")
+    print("--------- Character Count -------")  
+    for char_dict in sorted_characters:
+        char = char_dict["char"]
+        count = char_dict["num"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+    print("============= END ===============")
 
-    print (word_counts, character_counts)
+if __name__ == "__main__":
+    main()
